1dacb778a324d617619d9ec1f4fa591bb41e87ce
Din Music <din.music@canonical.com>
2024-12-05 13:07:02 +0000

    lxd/storage/drivers/pure: Add NVMe/TCP support
    Signed-off-by: Din Music <din.music@canonical.com>

--- lxd/storage/drivers/driver_pure_util.go
+++ lxd/storage/drivers/driver_pure_util.go
@@ -39,6 +39,7 @@ const pureAPIVersion = "2.21"
 // service name.
 var pureServiceNameMapping = map[string]string{
 	pureModeISCSI: "iscsi",
+	pureModeNVMe:  "nvme-tcp",
 }
 
 // pureVolTypePrefixes maps volume type to storage volume name prefix.
@@ -159,6 +160,7 @@ type pureVolume struct {
 type pureHost struct {
 	Name            string   `json:"name"`
 	IQNs            []string `json:"iqns"`
+	NQNs            []string `json:"nqns"`
 	ConnectionCount int      `json:"connection_count"`
 }
 
@@ -621,8 +623,14 @@ func (p *pureClient) getCurrentHost() (*pureHost, error) {
 		return nil, err
 	}
 
+	mode := p.driver.config["pure.mode"]
+
 	for _, host := range hosts {
-		if slices.Contains(host.IQNs, qn) {
+		if mode == pureModeISCSI && slices.Contains(host.IQNs, qn) {
+			return &host, nil
+		}
+
+		if mode == pureModeNVMe && slices.Contains(host.NQNs, qn) {
 			return &host, nil
 		}
 	}
@@ -639,6 +647,8 @@ func (p *pureClient) createHost(hostName string, qns []string) error {
 	switch mode {
 	case pureModeISCSI:
 		body["iqns"] = qns
+	case pureModeNVMe:
+		body["nqns"] = qns
 	default:
 		return fmt.Errorf("Unsupported Pure Storage mode %q", mode)
 	}
@@ -669,6 +679,8 @@ func (p *pureClient) updateHost(hostName string, qns []string) error {
 	switch mode {
 	case pureModeISCSI:
 		body["iqns"] = qns
+	case pureModeNVMe:
+		body["nqns"] = qns
 	default:
 		return fmt.Errorf("Unsupported Pure Storage mode %q", mode)
 	}
@@ -780,6 +792,10 @@ func (p *pureClient) getTarget() (targetQN string, targetAddrs []string, err err
 
 		if mode == pureModeISCSI {
 			nq = port.IQN
+		}
+
+		if mode == pureModeNVMe {
+			nq = port.NQN
 		}
 
 		if nq != "" {
@@ -905,6 +921,38 @@ func (d *pure) connect() (cleanup revert.Hook, err error) {
 			return nil
 		}
 
+	case pureModeNVMe:
+		// Try to find an existing NVMe session.
+		activeTargetNQN, err := nvmeFindSession(targetQN)
+		if err != nil {
+			return nil, err
+		}
+
+		if activeTargetNQN != "" {
+			// Already connected to the Pure Storage array via NVMe.
+			cleanup := revert.Clone().Fail
+			revert.Success()
+			return cleanup, nil
+		}
+
+		// Get the host's qualified name.
+		hostQN, err := d.hostQN()
+		if err != nil {
+			return nil, err
+		}
+
+		serverUUID := d.state.ServerUUID
+
+		// Function that connects to the NVMe target on a given address.
+		connectFunc = func(ctx context.Context, addr string) error {
+			_, _, err := shared.RunCommandSplit(ctx, nil, nil, "nvme", "connect", "--transport", "tcp", "--traddr", addr, "--nqn", targetQN, "--hostnqn", hostQN, "--hostid", serverUUID)
+			if err != nil {
+				return fmt.Errorf("Failed to connect to Pure Storage array via NVMe: %w", err)
+			}
+
+			return nil
+		}
+
 	default:
 		return nil, fmt.Errorf("Unsupported Pure Storage mode %q", d.config["pure.mode"])
 	}
@@ -1003,6 +1051,21 @@ func (d *pure) disconnect(targetQN string) error {
 				return fmt.Errorf("Failed disconnecting from Pure Storage iSCSI target %q: %w", targetQN, err)
 			}
 		}
+
+	case pureModeNVMe:
+		targetNQN, err := nvmeFindSession(targetQN)
+		if err != nil {
+			return err
+		}
+
+		if targetNQN != "" {
+			// Disconnect from the NVMe target.
+			_, err := shared.RunCommand("nvme", "disconnect", "--nqn", targetNQN)
+			if err != nil {
+				return fmt.Errorf("Failed disconnecting from Pure Storage NVMe target %q: %w", targetNQN, err)
+			}
+		}
+
 	default:
 		return fmt.Errorf("Unsupported Pure Storage mode %q", d.config["pure.mode"])
 	}
@@ -1238,6 +1301,20 @@ func (d *pure) getMappedDevPath(vol Volume, mapVolume bool) (string, revert.Hook
 	case pureModeISCSI:
 		diskPrefix = "scsi-"
 		diskSuffix = strings.ToLower(pureVol.Serial)
+	case pureModeNVMe:
+		diskPrefix = "nvme-eui."
+
+		// The serial number is used to identify the device. The last 10 characters
+		// of the serial number appear as a disk device suffix. This check ensures
+		// we do not panic if the reported serial number is too short for parsing.
+		if len(pureVol.Serial) <= 10 {
+			// Serial number is too short.
+			return "", nil, fmt.Errorf("Failed to locate device for volume %q: Invalid serial number %q", vol.name, pureVol.Serial)
+		}
+
+		// Extract the last 10 characters of the serial number. Also convert
+		// it to lower case, as on host the device ID is completely lower case.
+		diskSuffix = strings.ToLower(pureVol.Serial[len(pureVol.Serial)-10:])
 	default:
 		return "", nil, fmt.Errorf("Unsupported Pure Storage mode %q", d.config["pure.mode"])
 	}
@@ -1321,6 +1398,41 @@ func iscsiFindSession(iqn string) (string, error) {
 	return "", nil
 }
 
+// nvmeFindSession returns the given NQN if it corresponds to an existing session.
+// If the session is not found, an empty string is returned.
+func nvmeFindSession(nqn string) (string, error) {
+	// Base path for NVMe sessions/subsystems.
+	basePath := "/sys/devices/virtual/nvme-subsystem"
+
+	// Retrieve list of existing NVMe sessions on this host.
+	directories, err := os.ReadDir(basePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			// No active sessions because NVMe subsystems directory does not exist.
+			return "", nil
+		}
+
+		return "", fmt.Errorf("Failed getting a list of existing NVMe subsystems: %w", err)
+	}
+
+	for _, directory := range directories {
+		subsystemName := directory.Name()
+
+		// Get the target NQN.
+		nqnBytes, err := os.ReadFile(filepath.Join(basePath, subsystemName, "subsysnqn"))
+		if err != nil {
+			return "", fmt.Errorf("Failed getting the target NQN for subystem %q: %w", subsystemName, err)
+		}
+
+		if strings.Contains(string(nqnBytes), nqn) {
+			// Already connected to the Pure Storage array via NVMe.
+			return nqn, nil
+		}
+	}
+
+	return "", nil
+}
+
 // hostQN returns the qualified name for the current host based on the configured mode.
 func (d *pure) hostQN() (string, error) {
 	switch d.config["pure.mode"] {
@@ -1348,6 +1460,11 @@ func (d *pure) hostQN() (string, error) {
 		}
 
 		return "", fmt.Errorf(`Failed to extract host IQN: File %q does not contain "InitiatorName"`, filename)
+	case pureModeNVMe:
+		// Generate custom NQN from the server UUID. Getting the NQN from
+		// /etc/nvme/hostnqn would require the nvme-cli package to be
+		// installed on the host.
+		return fmt.Sprintf("nqn.2014-08.org.nvmexpress:uuid:%s", d.state.ServerUUID), nil
 	default:
 		return "", fmt.Errorf("Unsupported Pure Storage mode %q", d.config["pure.mode"])
 	}
@@ -1396,6 +1513,18 @@ func (d *pure) getVolumeName(vol Volume) (string, error) {
 	}
 
 	return volName, nil
+}
+
+// loadNVMeModules loads the NVMe/TCP kernel modules.
+// Returns true if the modules can be loaded.
+func (d *pure) loadNVMeModules() bool {
+	err := util.LoadModule("nvme_fabrics")
+	if err != nil {
+		return false
+	}
+
+	err = util.LoadModule("nvme_tcp")
+	return err == nil
 }
 
 // loadISCSIModules loads the iSCSI kernel modules.
