0605ecd82c8c6b53ff41f31745ed894a9b22b150
Din Music <din.music@canonical.com>
2025-04-03 11:32:54 +0000

    lxd-agent/devlxd: Add APIExtensions to /1.0 endpoint
    Signed-off-by: Din Music <din.music@canonical.com>

--- lxd/devlxd.go
+++ lxd/devlxd.go
@@ -155,6 +155,7 @@ func devLXDAPIGetHandler(d *Daemon, r *http.Request) response.Response {
 
 	resp := api.DevLXDGet{
 		APIVersion:              version.APIVersion,
+		APIExtensions:           version.APIExtensions,
 		Location:                location,
 		InstanceType:            inst.Type().String(),
 		SupportedStorageDrivers: supportedDrivers,
