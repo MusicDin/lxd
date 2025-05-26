4e5fc015db7ca18927cd0e5b74842a649c89f92e
Din Music <din.music@canonical.com>
2025-05-26 10:10:46 +0000

    lxd/devlxd: Get storage pool volumes endpoint
    Signed-off-by: Din Music <din.music@canonical.com>

--- lxd/devlxd.go
+++ lxd/devlxd.go
@@ -85,6 +85,7 @@ var apiDevLXD = []devLXDAPIEndpoint{
 	devLXDDevicesEndpoint,
 	devLXDStoragePoolEndpoint,
 	devLXDStoragePoolsEndpoint,
+	devLXDStoragePoolVolumesTypeEndpoint,
 	devLXDUbuntuProEndpoint,
 	devLXDUbuntuProTokenEndpoint,
 }
