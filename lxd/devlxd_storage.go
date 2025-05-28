239f8185bbeac01e9dd5ff943c873518a82f8103
Din Music <din.music@canonical.com>
2025-05-28 14:03:42 +0000

    lxd/devlxd: Patch storage pool volume endpoint
    Signed-off-by: Din Music <din.music@canonical.com>

--- lxd/devlxd_storage.go
+++ lxd/devlxd_storage.go
@@ -223,6 +223,7 @@ var devLXDStoragePoolVolumeTypeEndpoint = devLXDAPIEndpoint{
 	Path:   "storage-pools/{poolName}/volumes/{type}/{volumeName}",
 	Get:    devLXDAPIEndpointAction{Handler: devLXDStoragePoolVolumeGetHandler, AllowUntrusted: true},
 	Put:    devLXDAPIEndpointAction{Handler: devLXDStoragePoolVolumePutHandler, AllowUntrusted: true},
+	Patch:  devLXDAPIEndpointAction{Handler: devLXDStoragePoolVolumePutHandler, AllowUntrusted: true},
 	Delete: devLXDAPIEndpointAction{Handler: devLXDStoragePoolVolumeDeleteHandler, AllowUntrusted: true},
 }
 
@@ -327,7 +328,13 @@ func devLXDStoragePoolVolumePutHandler(d *Daemon, r *http.Request) response.Resp
 		response.DevLXDErrorResponse(err)
 	}
 
-	resp := storagePoolVolumePut(d, req)
+	var resp response.Response
+	if r.Method == http.MethodPatch {
+		resp = storagePoolVolumePatch(d, req)
+	} else {
+		resp = storagePoolVolumePut(d, req)
+	}
+
 	err = Render(req, resp)
 	if err != nil {
 		return response.DevLXDErrorResponse(err)
