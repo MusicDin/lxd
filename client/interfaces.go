f31451209c3621f62747ae2d37976ec8535a6e37
Din Music <din.music@canonical.com>
2025-06-12 10:49:27 +0000

    client/devlxd: Delete instance devices
    Signed-off-by: Din Music <din.music@canonical.com>

--- client/interfaces.go
+++ client/interfaces.go
@@ -504,6 +504,7 @@ type DevLXDServer interface {
 	GetInstanceDevices(instName string) (devices map[string]map[string]string, err error)
 	GetInstanceDevice(instName string, deviceName string) (device map[string]string, etag string, err error)
 	CreateInstanceDevice(instName string, device map[string]string) (err error)
+	DeleteInstanceDevice(instName string, deviceName string) error
 
 	// DevLXD storage pools.
 	GetStoragePools() (pools []api.DevLXDStoragePool, err error)
