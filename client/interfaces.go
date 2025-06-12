0446b70aa962f122dee4c6cc6ee90e3edfe268e4
Din Music <din.music@canonical.com>
2025-06-12 10:49:27 +0000

    client/devlxd: Get instance
    Signed-off-by: Din Music <din.music@canonical.com>

--- client/interfaces.go
+++ client/interfaces.go
@@ -506,6 +506,9 @@ type DevLXDServer interface {
 	// DevLXD Operations.
 	GetOperationWait(uuid string, timeout int) (op *api.DevLXDOperation, ETag string, err error)
 
+	// DevLXD instance devices.
+	GetInstance(instName string) (inst *api.DevLXDInstance, etag string, err error)
+
 	// DevLXD storage pools.
 	GetStoragePools() (pools []api.DevLXDStoragePool, err error)
 	GetStoragePool(poolName string) (pool *api.DevLXDStoragePool, ETag string, err error)
