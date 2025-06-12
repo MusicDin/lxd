2d35da16a204504bfbdaff6877037c478e1bad29
Din Music <din.music@canonical.com>
2025-06-12 10:49:27 +0000

    client/devlxd: Get instance device
    Signed-off-by: Din Music <din.music@canonical.com>

--- client/devlxd_instances.go
+++ client/devlxd_instances.go
@@ -18,3 +18,17 @@ func (r *ProtocolDevLXD) GetInstanceDevices(instName string) (devices map[string
 
 	return devices, nil
 }
+
+// GetInstanceDevice retrieves a specific instance device.
+// It also returns the ETag of an instance, which is used when updating the instance devices.
+func (r *ProtocolDevLXD) GetInstanceDevice(instName string, deviceName string) (device map[string]string, etag string, err error) {
+	device = make(map[string]string)
+
+	url := api.NewURL().Path("instances", instName, "devices", deviceName).URL
+	etag, err = r.queryStruct(http.MethodGet, url.String(), nil, "", &device)
+	if err != nil {
+		return nil, "", err
+	}
+
+	return device, etag, nil
+}
