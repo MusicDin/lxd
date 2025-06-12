07a79a19403f2c2e8044380e83cc049d926d504c
Din Music <din.music@canonical.com>
2025-06-12 10:07:48 +0000

    lxd-agent/devlxd: Create instance device endpoint
    Signed-off-by: Din Music <din.music@canonical.com>

--- lxd-agent/devlxd_instance.go
+++ lxd-agent/devlxd_instance.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/url"
 
@@ -10,6 +11,7 @@ import (
 var devLXDInstanceDevicesEndpoint = devLXDAPIEndpoint{
 	Path: "instances/{instanceName}/devices",
 	Get:  devLXDAPIEndpointAction{Handler: devLXDInstanceDevicesGetHandler},
+	Post: devLXDAPIEndpointAction{Handler: devLXDInstanceDevicesPostHandler},
 }
 
 func devLXDInstanceDevicesGetHandler(d *Daemon, r *http.Request) *devLXDResponse {
@@ -31,4 +33,31 @@ func devLXDInstanceDevicesGetHandler(d *Daemon, r *http.Request) *devLXDResponse
 	}
 
 	return okResponse(devices, "json")
+}
+
+func devLXDInstanceDevicesPostHandler(d *Daemon, r *http.Request) *devLXDResponse {
+	instName, err := url.PathUnescape(mux.Vars(r)["instanceName"])
+	if err != nil {
+		return errorResponse(http.StatusBadRequest, err.Error())
+	}
+
+	var device map[string]string
+	err = json.NewDecoder(r.Body).Decode(&device)
+	if err != nil {
+		return smartResponse(err)
+	}
+
+	client, err := getDevLXDVsockClient(d)
+	if err != nil {
+		return smartResponse(err)
+	}
+
+	defer client.Disconnect()
+
+	err = client.CreateInstanceDevice(instName, device)
+	if err != nil {
+		return smartResponse(err)
+	}
+
+	return okResponse("", "raw")
 }
