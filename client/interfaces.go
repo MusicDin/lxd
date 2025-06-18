36a4d06c7591a1f01c4a3cda66568a9ac47cc443
Din Music <din.music@canonical.com>
2025-06-18 14:49:24 +0000

    client/devlxd: Get storage pool volume snapshot
    Signed-off-by: Din Music <din.music@canonical.com>

--- client/interfaces.go
+++ client/interfaces.go
@@ -519,6 +519,7 @@ type DevLXDServer interface {
 
 	// DevLXD storage volume snapshots.
 	GetStoragePoolVolumeSnapshots(poolName string, volType string, volName string) (snapshots []api.DevLXDStorageVolumeSnapshot, err error)
+	GetStoragePoolVolumeSnapshot(poolName string, volType string, volName string, snapshotName string) (snapshot *api.DevLXDStorageVolumeSnapshot, ETag string, err error)
 
 	// DevLXD Ubuntu Pro.
 	GetUbuntuPro() (*api.UbuntuProSettings, error)
