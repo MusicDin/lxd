5dbdaade652e175ea06f1494c0106a57192eadc3
Din Music <din.music@canonical.com>
2025-07-10 12:25:11 +0000

    lxd-agent/devlxd: Update instance endpoint
    Signed-off-by: Din Music <din.music@canonical.com>

--- lxd-agent/devlxd_instances.go
+++ lxd-agent/devlxd_instances.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/url"
 
+	"github.com/canonical/lxd/shared/api"
 	"github.com/gorilla/mux"
 )
 
 var devLXDInstanceEndpoint = devLXDAPIEndpoint{
-	Path: "instances/{name}",
-	Get:  devLXDAPIEndpointAction{Handler: devLXDInstanceGetHandler},
+	Path:  "instances/{name}",
+	Get:   devLXDAPIEndpointAction{Handler: devLXDInstanceGetHandler},
+	Patch: devLXDAPIEndpointAction{Handler: devLXDInstancePatchHandler},
 }
 
 func devLXDInstanceGetHandler(d *Daemon, r *http.Request) *devLXDResponse {
@@ -31,4 +34,32 @@ func devLXDInstanceGetHandler(d *Daemon, r *http.Request) *devLXDResponse {
 	}
 
 	return okResponseETag(inst, "json", etag)
+}
+
+func devLXDInstancePatchHandler(d *Daemon, r *http.Request) *devLXDResponse {
+	instName, err := url.PathUnescape(mux.Vars(r)["name"])
+	if err != nil {
+		return errorResponse(http.StatusBadRequest, err.Error())
+	}
+
+	var inst api.DevLXDInstance
+	err = json.NewDecoder(r.Body).Decode(&inst)
+	if err != nil {
+		return smartResponse(err)
+	}
+
+	client, err := getDevLXDVsockClient(d)
+	if err != nil {
+		return smartResponse(err)
+	}
+
+	defer client.Disconnect()
+
+	etag := r.Header.Get("If-Match")
+	err = client.UpdateInstance(instName, inst, etag)
+	if err != nil {
+		return smartResponse(err)
+	}
+
+	return okResponse("", "raw")
 }
