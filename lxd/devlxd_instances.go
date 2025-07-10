1f4787eb77b2a5c11a85905610a2ca1dc85ce21c
Din Music <din.music@canonical.com>
2025-07-10 12:10:05 +0000

    lxd/devlxd: Helper function to extract accessible devices
    Signed-off-by: Din Music <din.music@canonical.com>

--- lxd/devlxd_instances.go
+++ lxd/devlxd_instances.go
@@ -3,9 +3,34 @@ package main
 import (
 	"github.com/canonical/lxd/lxd/device/filters"
 	"github.com/canonical/lxd/lxd/instance"
+	"github.com/canonical/lxd/shared/api"
 )
 
 type devLXDDeviceAccessValidator func(device map[string]string) bool
+
+// getAccessibleDevices extracts accessible instance devices. Two maps are returned,
+// one containg accessible devices, and the other containing the remaining devices.
+//
+// Device is accessible if:
+// - Device owner matches the service account ID.
+// - Device type matches the allowed device types.
+//
+// Additionall restrictions::
+// - Disk device is accessible only if it is a custom volume disk device.
+func getAccessibleDevices(inst api.Instance, serviceAccountID string, isDeviceAccessible devLXDDeviceAccessValidator) (accessibleDevices map[string]map[string]string, otherDevices map[string]map[string]string) {
+	accessibleDevices = make(map[string]map[string]string)
+	otherDevices = make(map[string]map[string]string)
+
+	for name, device := range inst.Devices {
+		if isDeviceAccessible != nil && isDeviceAccessible(device) && inst.Config["volatile."+name+".devlxd.owner"] == serviceAccountID {
+			accessibleDevices[name] = device
+		} else {
+			otherDevices[name] = device
+		}
+	}
+
+	return accessibleDevices, otherDevices
+}
 
 // newDevLXDDeviceAccessValidator returns a device validator function that
 // checks if the given device is accessible by the devLXD.
