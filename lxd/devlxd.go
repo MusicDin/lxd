1e1ace7b160da5b2cb28323098e6e733e4cab204
Din Music <din.music@canonical.com>
2025-08-20 15:07:47 +0000

    lxd/devlxd: Enable AllowUntrusted for existing devLXD endpoints
    Signed-off-by: Din Music <din.music@canonical.com>

--- lxd/devlxd.go
+++ lxd/devlxd.go
@@ -83,6 +83,7 @@ var apiDevLXD = []devLXDAPIEndpoint{
 			Handler: func(d *Daemon, r *http.Request) response.Response {
 				return response.DevLXDResponse(http.StatusOK, []string{"/1.0"}, "json")
 			},
+			AllowUntrusted: true,
 		},
 	},
 	devLXD10Endpoint,
@@ -98,8 +99,8 @@ var apiDevLXD = []devLXDAPIEndpoint{
 
 var devLXD10Endpoint = devLXDAPIEndpoint{
 	Path:  "",
-	Get:   devLXDAPIEndpointAction{Handler: devLXDAPIGetHandler},
-	Patch: devLXDAPIEndpointAction{Handler: devLXDAPIPatchHandler},
+	Get:   devLXDAPIEndpointAction{Handler: devLXDAPIGetHandler, AllowUntrusted: true},
+	Patch: devLXDAPIEndpointAction{Handler: devLXDAPIPatchHandler, AllowUntrusted: true},
 }
 
 func devLXDAPIGetHandler(d *Daemon, r *http.Request) response.Response {
@@ -187,7 +188,7 @@ func devLXDAPIPatchHandler(d *Daemon, r *http.Request) response.Response {
 
 var devLXDConfigEndpoint = devLXDAPIEndpoint{
 	Path: "config",
-	Get:  devLXDAPIEndpointAction{Handler: devLXDConfigGetHandler},
+	Get:  devLXDAPIEndpointAction{Handler: devLXDConfigGetHandler, AllowUntrusted: true},
 }
 
 func devLXDConfigGetHandler(d *Daemon, r *http.Request) response.Response {
@@ -238,7 +239,7 @@ func devLXDConfigGetHandler(d *Daemon, r *http.Request) response.Response {
 
 var devLXDConfigKeyEndpoint = devLXDAPIEndpoint{
 	Path: "config/{key}",
-	Get:  devLXDAPIEndpointAction{Handler: devLXDConfigKeyGetHandler},
+	Get:  devLXDAPIEndpointAction{Handler: devLXDConfigKeyGetHandler, AllowUntrusted: true},
 }
 
 func devLXDConfigKeyGetHandler(d *Daemon, r *http.Request) response.Response {
@@ -284,7 +285,7 @@ func devLXDConfigKeyGetHandler(d *Daemon, r *http.Request) response.Response {
 
 var devLXDImageExportEndpoint = devLXDAPIEndpoint{
 	Path: "images/{fingerprint}/export",
-	Get:  devLXDAPIEndpointAction{Handler: devLXDImageExportHandler},
+	Get:  devLXDAPIEndpointAction{Handler: devLXDImageExportHandler, AllowUntrusted: true},
 }
 
 // devLXDImageExportHandler returns a file response containing the image files. The requested fingerprint must match
@@ -348,7 +349,7 @@ func devLXDImageExportHandler(d *Daemon, r *http.Request) response.Response {
 
 var devLXDMetadataEndpoint = devLXDAPIEndpoint{
 	Path: "meta-data",
-	Get:  devLXDAPIEndpointAction{Handler: devLXDMetadataGetHandler},
+	Get:  devLXDAPIEndpointAction{Handler: devLXDMetadataGetHandler, AllowUntrusted: true},
 }
 
 func devLXDMetadataGetHandler(d *Daemon, r *http.Request) response.Response {
@@ -364,7 +365,7 @@ func devLXDMetadataGetHandler(d *Daemon, r *http.Request) response.Response {
 
 var devLXDEventsEndpoint = devLXDAPIEndpoint{
 	Path: "events",
-	Get:  devLXDAPIEndpointAction{Handler: devLXDEventsGetHandler},
+	Get:  devLXDAPIEndpointAction{Handler: devLXDEventsGetHandler, AllowUntrusted: true},
 }
 
 func devLXDEventsGetHandler(d *Daemon, r *http.Request) response.Response {
@@ -430,7 +431,7 @@ func devLXDEventsGetHandler(d *Daemon, r *http.Request) response.Response {
 
 var devLXDDevicesEndpoint = devLXDAPIEndpoint{
 	Path: "devices",
-	Get:  devLXDAPIEndpointAction{Handler: devLXDDevicesGetHandler},
+	Get:  devLXDAPIEndpointAction{Handler: devLXDDevicesGetHandler, AllowUntrusted: true},
 }
 
 func devLXDDevicesGetHandler(d *Daemon, r *http.Request) response.Response {
@@ -455,7 +456,7 @@ func devLXDDevicesGetHandler(d *Daemon, r *http.Request) response.Response {
 
 var devLXDUbuntuProEndpoint = devLXDAPIEndpoint{
 	Path: "ubuntu-pro",
-	Get:  devLXDAPIEndpointAction{Handler: devLXDUbuntuProGetHandler},
+	Get:  devLXDAPIEndpointAction{Handler: devLXDUbuntuProGetHandler, AllowUntrusted: true},
 }
 
 func devLXDUbuntuProGetHandler(d *Daemon, r *http.Request) response.Response {
@@ -472,7 +473,7 @@ func devLXDUbuntuProGetHandler(d *Daemon, r *http.Request) response.Response {
 
 var devLXDUbuntuProTokenEndpoint = devLXDAPIEndpoint{
 	Path: "ubuntu-pro/token",
-	Post: devLXDAPIEndpointAction{Handler: devLXDUbuntuProTokenPostHandler},
+	Post: devLXDAPIEndpointAction{Handler: devLXDUbuntuProTokenPostHandler, AllowUntrusted: true},
 }
 
 func devLXDUbuntuProTokenPostHandler(d *Daemon, r *http.Request) response.Response {
