ab71dcf6419519f5803812d32b705c6ecaff7070
Din Music <din.music@canonical.com>
2026-01-21 14:22:52 +0000

    ATTEMPT: cross-lxd live migration
    Signed-off-by: Din Music <din.music@canonical.com>

--- test/suites/live_migration.sh
+++ test/suites/live_migration.sh
@@ -3,7 +3,7 @@
 # test_clustering_live_migration spawns a 2-node LXD cluster, creates a virtual machine on top of it,
 # creates and attaches a block volume to the virtual machine, writes some arbitrary data to the volume,
 # and runs live migration. Success is determined by the data being intact after live migration.
-test_clustering_live_migration() {
+test_clustering_live_migration_intra_cluster() {
   poolDriver="$(storage_backend "${LXD_INITIAL_DIR}")"
   if [ "${poolDriver}" = "lvm" ]; then
     export TEST_UNMET_REQUIREMENT="Storage driver ${poolDriver} is currently unsupported"
@@ -119,6 +119,119 @@ test_clustering_live_migration() {
 
   teardown_clustering_netns
   teardown_clustering_bridge
+
+  kill_lxd "${LXD_ONE_DIR}"
+  kill_lxd "${LXD_TWO_DIR}"
+}
+
+# test_clustering_live_migration spawns a 2-node LXD cluster, creates a virtual machine on top of it,
+# creates and attaches a block volume to the virtual machine, writes some arbitrary data to the volume,
+# and runs live migration. Success is determined by the data being intact after live migration.
+test_clustering_live_migration() {
+  poolDriver="$(storage_backend "${LXD_INITIAL_DIR}")"
+
+  # For remote storage drivers, we perform the live migration with custom storage pool attached as well.
+  isRemoteDriver=false
+  if [ "${poolDriver}" == "ceph" ]; then
+    isRemoteDriver=true
+  fi
+
+  LXD_ONE_DIR="$(mktemp -d -p "${TEST_DIR}" XXX)"
+  LXD_TWO_DIR="$(mktemp -d -p "${TEST_DIR}" XXX)"
+
+    # Spawn a second LXD.
+  if [ "${poolDriver}" == "ceph" ]; then
+    LXD_TEST_LIVE_MIGRATION_ON_THE_SAME_HOST=true spawn_lxd "${LXD_ONE_DIR}" true
+  else
+    spawn_lxd "${LXD_ONE_DIR}" true
+  fi
+
+  # Spawn a second LXD.
+  if [ "${poolDriver}" == "ceph" ]; then
+    LXD_TEST_LIVE_MIGRATION_ON_THE_SAME_HOST=true spawn_lxd "${LXD_TWO_DIR}" true
+  else
+    spawn_lxd "${LXD_TWO_DIR}" true
+  fi
+
+  # Set up a TLS identity with admin permissions.
+  LXD_DIR="${LXD_ONE_DIR}" lxc auth group create live-migration
+  LXD_DIR="${LXD_ONE_DIR}" lxc auth group permission add live-migration server admin
+
+  token="$(LXD_DIR="${LXD_ONE_DIR}" lxc auth identity create tls/live-migration --group=live-migration --quiet)"
+  LXD_DIR="${LXD_ONE_DIR}" lxc remote add dst 100.64.1.101:8443 --token="${token}"
+
+  LXD_DIR="${LXD_ONE_DIR}" ensure_import_ubuntu_vm_image
+
+  # Storage pool created when spawning LXD cluster is "data".
+  poolName="data"
+  LXD_DIR="${LXD_ONE_DIR}" lxc storage set "${poolName}" volume.size="${SMALLEST_VM_ROOT_DISK}"
+
+  # Initialize the VM.
+  LXD_DIR="${LXD_ONE_DIR}" lxc init ubuntu-vm vm \
+    --vm \
+    --config limits.cpu=2 \
+    --config limits.memory=768MiB \
+    --config migration.stateful=true \
+    --device root,size="${SMALLEST_VM_ROOT_DISK}" \
+    --target node1
+
+  # For remote storage drivers, test live migration with custom volume as well.
+  if [ "${isRemoteDriver}" = true ]; then
+    # Attach the block volume to the VM.
+    LXD_DIR="${LXD_ONE_DIR}" lxc storage volume create "${poolName}" vmdata --type=block size=1MiB
+    LXD_DIR="${LXD_ONE_DIR}" lxc config device add vm vmdata disk pool="${poolName}" source=vmdata
+  fi
+
+  # Start the VM.
+  LXD_DIR="${LXD_ONE_DIR}" lxc start vm
+  LXD_DIR="${LXD_ONE_DIR}" waitInstanceReady vm
+
+  # Inside the VM, format and mount the volume, then write some data to it.
+  if [ "${isRemoteDriver}" = true ]; then
+    LXD_DIR="${LXD_ONE_DIR}" lxc exec vm -- mkfs -t ext4 /dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_lxd_vmdata
+    LXD_DIR="${LXD_ONE_DIR}" lxc exec vm -- mkdir /mnt/vol1
+    LXD_DIR="${LXD_ONE_DIR}" lxc exec vm -- mount -t ext4 /dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_lxd_vmdata /mnt/vol1
+    LXD_DIR="${LXD_ONE_DIR}" lxc exec vm -- cp /etc/hostname /mnt/vol1/bar
+  fi
+
+  # Perform live migration of the VM from node1 to node2.
+  echo "Live migrating instance 'vm' ..."
+  LXD_DIR="${LXD_ONE_DIR}" lxc move vm dst:vm
+  LXD_DIR="${LXD_TWO_DIR}" waitInstanceReady vm
+
+  # After live migration, the volume should be functional and mounted.
+  # Check that the file we created is still there with the same contents.
+  echo "Verifying data integrity after live migration"
+  if [ "${isRemoteDriver}" = true ]; then
+    [ "$(LXD_DIR="${LXD_TWO_DIR}" lxc exec vm -- cat /mnt/vol1/bar)" = "vm" ]
+  fi
+
+  # Cleanup
+  echo "Cleaning up ..."
+  LXD_DIR="${LXD_ONE_DIR}" lxc image delete "$(LXD_DIR="${LXD_ONE_DIR}" lxc config get vm volatile.base_image)"
+  LXD_DIR="${LXD_TWO_DIR}" lxc delete --force vm
+
+  if [ "${isRemoteDriver}" = true ]; then
+    LXD_DIR="${LXD_TWO_DIR}" lxc storage volume delete "${poolName}" vmdata
+  fi
+
+  # Ensure cleanup of the cluster's data pool to not leave any traces behind when we are using a different driver besides dir.
+  printf 'config: {}\ndevices: {}' | LXD_DIR="${LXD_ONE_DIR}" lxc profile edit default
+  LXD_DIR="${LXD_ONE_DIR}" lxc storage delete "${poolName}"
+  LXD_DIR="${LXD_TWO_DIR}" lxc storage delete "${poolName}"
+
+  # DEBUG: Show storage pool info
+  echo ">>> DEBUG AFTER CLEANUP <<<"
+  LXD_DIR="${LXD_ONE_DIR}" lxc storage ls || true
+  LXD_DIR="${LXD_TWO_DIR}" lxc storage ls || true
+  LXD_DIR="${LXD_ONE_DIR}" lxc image ls || true
+  LXD_DIR="${LXD_TWO_DIR}" lxc image ls || true
+  # DEBUG: END
+
+  lxc remote remove dst
+
+  LXD_DIR="${LXD_ONE_DIR}" lxd shutdown
+  LXD_DIR="${LXD_TWO_DIR}" lxd shutdown
 
   kill_lxd "${LXD_ONE_DIR}"
   kill_lxd "${LXD_TWO_DIR}"
