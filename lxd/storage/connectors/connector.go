6dd87177a3980e7193ab3ebfaa48b3a29200adc0
Din Music <din.music@canonical.com>
2026-05-05 08:56:28 +0000

    lxd/storage/connectors: Implement SCSI/FC connector
    Signed-off-by: Din Music <din.music@canonical.com>

--- lxd/storage/connectors/connector.go
+++ lxd/storage/connectors/connector.go
@@ -20,6 +20,9 @@ const (
 
 	// TypeISCSI represents an iSCSI storage connector.
 	TypeISCSI string = "iscsi"
+
+	// TypeSCSIFC represents an SCSI over Fibre Channel storage connector.
+	TypeSCSIFC string = "scsi-fc"
 )
 
 // session represents a connector session that is established with a target.
@@ -72,6 +75,11 @@ func NewConnector(connectorType string, serverUUID string) (Connector, error) {
 
 	case TypeISCSI:
 		return &connectorISCSI{
+			common: common,
+		}, nil
+
+	case TypeSCSIFC:
+		return &connectorSCSIFC{
 			common: common,
 		}, nil
 
