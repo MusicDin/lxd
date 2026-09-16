f97ce9a5911c998cd309e77d3daa6dcba9924f31
Din Music <din.music@canonical.com>
2026-09-16 11:53:04 +0000

    lxd/storage/connectors: Use shared WWN normalization func
    Signed-off-by: Din Music <din.music@canonical.com>

--- lxd/storage/connectors/connector_scsi_fc.go
+++ lxd/storage/connectors/connector_scsi_fc.go
@@ -104,7 +104,7 @@ func fcInitiatorWWPNs(fcHostPath string) ([]string, error) {
 			continue
 		}
 
-		wwpn := normalizeWWPN(string(portNameBytes))
+		wwpn := block.NormalizeWWN(string(portNameBytes))
 		if wwpn == "" || slices.Contains(wwpns, wwpn) {
 			continue
 		}
@@ -138,7 +138,7 @@ func (c *connectorSCSIFC) Connect(ctx context.Context, wwpn string, luns ...stri
 		return nil, errors.New("At least one LUN must be provided to connect to an FC target")
 	}
 
-	wwpn = normalizeWWPN(wwpn)
+	wwpn = block.NormalizeWWN(wwpn)
 
 	type scanTarget struct {
 		host    string
@@ -153,7 +153,7 @@ func (c *connectorSCSIFC) Connect(ctx context.Context, wwpn string, luns ...stri
 			continue
 		}
 
-		portName := normalizeWWPN(string(portNameBytes))
+		portName := block.NormalizeWWN(string(portNameBytes))
 		if portName != wwpn {
 			continue
 		}
@@ -248,18 +248,14 @@ func (c *connectorSCSIFC) Discover(ctx context.Context, wwpns ...string) ([]any,
 			continue
 		}
 
-		portName := normalizeWWPN(string(portNameBytes))
+		portName := block.NormalizeWWN(string(portNameBytes))
 
 		if len(wwpns) > 0 {
-			found := false
-			for _, wwpn := range wwpns {
-				if strings.EqualFold(portName, normalizeWWPN(wwpn)) {
-					found = true
-					break
-				}
-			}
+			portFound := slices.ContainsFunc(wwpns, func(wwpn string) bool {
+				return strings.EqualFold(portName, block.NormalizeWWN(wwpn))
+			})
 
-			if !found {
+			if !portFound {
 				continue
 			}
 		}
@@ -276,7 +272,7 @@ func (c *connectorSCSIFC) Discover(ctx context.Context, wwpns ...string) ([]any,
 		}
 
 		record := FCDiscoveryRecord{
-			PortName: normalizeWWPN(portName),
+			PortName: portName,
 		}
 
 		result = append(result, record)
@@ -471,15 +467,4 @@ func (c *connectorSCSIFC) WaitDiskDeviceResize(ctx context.Context, devicePath s
 	}
 
 	return block.WaitDiskDeviceResize(ctx, devicePath, newSizeBytes)
-}
-
-// normalizeWWPN normalizes the WWPN string to make it comparable regardless of the format
-// it's provided in. Linux sysfs reports WWPNs as "0x" with 16 hex chars ("0x210034800d7035b3"),
-// while storage array might report it using colon-separated byte format ("21:00:34:80:0d:70:35:b3").
-func normalizeWWPN(wwpn string) string {
-	wwpn = strings.TrimSpace(wwpn)
-	wwpn = strings.ToLower(wwpn)
-	wwpn = strings.TrimPrefix(wwpn, "0x")
-	wwpn = strings.ReplaceAll(wwpn, ":", "")
-	return wwpn
 }
