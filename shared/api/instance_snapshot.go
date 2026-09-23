1ee914c2c72815d4b544818cb47e78fa3bf88ca5
Din Music <din.music@canonical.com>
2026-09-23 23:27:49 +0000

    shared/api: Add the bitmap field to the snapshot request
    Signed-off-by: Din Music <din.music@canonical.com>

--- shared/api/instance_snapshot.go
+++ shared/api/instance_snapshot.go
@@ -29,6 +29,12 @@ type InstanceSnapshotsPost struct {
 	//
 	// API extension: instance_snapshot_multi_volume
 	DiskVolumesMode string `json:"disk_volumes_mode,omitempty" yaml:"disk_volumes_mode,omitempty"`
+
+	// Whether to create a dirty bitmap named after the snapshot on every block volume of the snapshot
+	// Example: true
+	//
+	// API extension: storage_volume_block_tracking
+	Bitmap bool `json:"bitmap,omitempty" yaml:"bitmap,omitempty"`
 }
 
 // InstanceSnapshotPost represents the fields required to rename/move a LXD instance snapshot.
